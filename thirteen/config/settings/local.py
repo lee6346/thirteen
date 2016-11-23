821ff1529cab56884303fd3971eac252982b246f
James Lee <james.i.lee.00@gmail.com>
2016-11-23 09:50:48 -0600

    started redis config

--- thirteen/config/settings/local.py
+++ thirteen/config/settings/local.py
@@ -15,3 +15,13 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
