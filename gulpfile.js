44c01ddadd99a7e5022bfa44792c52297598e0be
Isaiah Lee <leesisaiah@gmail.com>
2016-12-01 23:58:03 -0800

    update gulp to handle copy of library scripts, fonts, stylesheets. remove bower

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,47 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     watch = require('gulp-watch');
 
-
 var root = './';
 var assetsRoot = './assets/'
 var staticRoot = './thirteen/static/';
 
-var dependencyDirs = [
-    root + 'bower_components/',
-    root + 'node_modules/'
-];
+var npmDir = root + 'node_modules/';
 
 var staticDirs = {
     styles: staticRoot + 'styles/',
     images: staticRoot + 'images/',
+    fonts: staticRoot + 'fonts/',
     scripts: {
-
+        libraries: staticRoot + 'scripts/libraries/'
     }
 };
 
+var npm = {
+    scripts: [
+        npmDir + 'angular/angular.js',
+        npmDir + 'angular-ui-router/release/angular-ui-router.js',
+        npmDir + 'jquery/dist/jquery.js',
+        npmDir + 'bootstrap/dist/js/bootstrap.js',
+        npmDir + 'angular-ui-bootstrap/dist/ui-bootstrap-tpls.js'
+    ],
+    styles: [
+        npmDir + 'bootstrap/dist/css/bootstrap.css',
+        npmDir + 'font-awesome/css/font-awesome.css',
+        npmDir + 'roboto-fontface/css/roboto/roboto-fontface.css'
+    ],
+    fonts: [
+        npmDir + 'bootstrap/dist/fonts/',
+        npmDir + 'font-awesome/fonts/',
+        npmDir + 'roboto-fontface/fonts/Roboto/'
+    ]
+};
+
+
+
+
 // this is the default task that's run when you 
 // run the command 'gulp'
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['copy', 'watch']);
 
 gulp.task('clean', ['clean-static']);
 
@@ -34,14 +54,21 @@ gulp.task('clean-static', function () {
     return del(staticRoot + '**/*');
 });
 
-
+// removes everything from node_modules
 gulp.task('clean-dependencies', function () {
-    return del(dependencyDirs.map(function(dir) {
+    return del([npmDir].map(function (dir) {
         return dir + '**/*';
     }));
 });
 
-gulp.task('sass', function () {
+gulp.task('copy', [
+    'copy-custom-sass',
+    'copy-library-scripts',
+    'copy-library-styles',
+    'copy-fonts'
+]);
+
+gulp.task('copy-custom-sass', function () {
     gulp.src(assetsRoot + 'style/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('style.css'))
@@ -49,8 +76,34 @@ gulp.task('sass', function () {
         .pipe(gulp.dest(staticDirs.styles));
 });
 
+gulp.task('copy-library-scripts', function () {
+    gulp.src(npm.scripts)
+        //.pipe(concat('libraries.js'))
+        .pipe(gulp.dest(staticDirs.scripts.libraries));
+});
+
+gulp.task('copy-library-styles', function () {
+    gulp.src(npm.styles)
+        .pipe(concat('libraries.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest(staticDirs.styles));
+});
+
+gulp.task('copy-fonts', function () {
+    var fontDirExpressions = npm.fonts.map(function (fontDir) {
+        return fontDir + '**/*';
+    });
+
+    gulp.src(fontDirExpressions)
+        .pipe(gulp.dest(staticDirs.fonts));
+})
+
 gulp.task('watch', ['watch-sass']);
 
 gulp.task('watch-sass', function () {
-  gulp.watch(assetsRoot + 'style/**/*.scss', ['sass']);
+    gulp.watch(assetsRoot + 'style/**/*.scss', ['copy-custom-sass']);
+});
+
+gulp.task('watch-custom-scripts', function () {
+
 });
