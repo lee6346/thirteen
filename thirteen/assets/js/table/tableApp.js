f528c9e35d1d680e2162f49759695e954e4affda
Isaiah Lee <leesisaiah@gmail.com>
2016-12-18 04:22:14 -0800

    more done

--- thirteen/assets/js/table/tableApp.js
+++ thirteen/assets/js/table/tableApp.js
@@ -8,16 +8,26 @@ angular.module('ttTableApp')
         var cards = [
             {
                 suit: 0,
+                rank: 0
+            },
+            {
+                suit: 0,
                 rank: 1
-            }
+            },
+            {
+                suit: 0,
+                rank: 2
+            },
         ];
 
         var cardSpriteMapper = function(cardsList){
             cardsList.forEach(function(card){
-                card.x = rank*58;
-                card.y = suit*77;
+                card.x = card.rank*58;
+                card.y = card.suit*77;
             })
         };
 
         cardSpriteMapper(cards);
+
+        $scope.cards = cards;
     });
